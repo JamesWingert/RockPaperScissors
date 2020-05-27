bad545cf4b7abe482f025ad021a35d023e15e842
James <jameswingert1@gmail.com>
2020-05-27 13:48:38 -0700

    First commit, getting first statements added

---
+++ RockPaperScissors.py
@@ -0,0 +1,7 @@
+import random
+
+print("Hello and welcome to Rock, Paper, Scissors tournament./n"
+      "The tournament will be the best of 3 wins!/n"
+      "It will be you against our highly intelligent computer opponent!/n"
+      "Good luck!")
+
